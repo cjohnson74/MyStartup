e49c5b2f9d0ac891302da6fb11ea550ff1906e47
carsonjohnson <cjohnson74.tech@gmail.com>
2022-03-03 20:24:34 -0500

    * .gitignore: Recoding to fix bootstrap bug.
    * WiredSneakz.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * Sneaker.cs:
    * site.js:
    * WiredSneakz.csproj:
    * favicon.ico:
    * site.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Privacy.cshtml:
    * Error.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * HomeController.cs:
    * LICENSE:
    * LICENSE.txt:
    * launchSettings.json:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.js.map:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * bootstrap-reboot.css.map:
    * bootstrap-reboot.min.css:
    * additional-methods.js:
    * bootstrap.bundle.min.js.map:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ WiredSneakz/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using WiredSneakz.Models;
+
+namespace WiredSneakz.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Sneaker> sneakers = new List<Sneaker>();
+            // Connect to MySQL.
+            using (MySqlConnection connection = new MySqlConnection("server=localhost;user=root;database=wiredsneakz_db;port=3306;password=rootroot"))
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from Sneaker", connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // Extract your data
+                    Sneaker sneaker = new Sneaker();
+                    sneaker.Id = Convert.ToInt32(reader["id"]);
+                    sneaker.Name = reader["name"].ToString();
+                    sneaker.ReleaseDate = Convert.ToDateTime(reader["releaseDate"]);
+                    sneaker.Colorway = reader["colorway"].ToString();
+                    sneaker.Price = Convert.ToDecimal(reader["price"]);
+
+                    sneakers.Add(sneaker);
+                }
+                reader.Close();
+            }
+
+            return View(sneakers);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WiredSneakz/Models/Sneaker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WiredSneakz.Models
+{
+    public class Sneaker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Colorway { get; set; }
+        public decimal Price { get; set; }
+    }
+}
