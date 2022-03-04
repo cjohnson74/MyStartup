8af9e863c015f9c830d51d9fc14575dfeb94da9c
carsonjohnson <cjohnson74.tech@gmail.com>
2022-03-04 14:07:21 -0500

    * Program.cs: * Index.cshtml:
    * WiredSneakz.csproj: "added HtmlAgilityPack and CsvHelper to assist
      with web scraping"

--- WiredSneakz/Program.cs
+++ WiredSneakz/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using HtmlAgilityPack;
+using CsvHelper;
 
 namespace WiredSneakz
 {
@@ -22,5 +24,14 @@ namespace WiredSneakz
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        // Parses the URL and returns HtmlDocument object
+        // static HtmlDocument GetDocument(string url)
+        static HtmlDocument GetDocument(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            return doc;
+        }
     }
 }
